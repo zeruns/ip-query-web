722ec475eb0b769c5c70620f79179135b15f37a4
zeruns <676672384@qq.com>
2026-05-26 19:35:30 +0800

    Referrer-Policy改为strict-origin-when-cross-origin，允许百度统计获取来源信息

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('X-XSS-Protection', '1; mode=block');
-  res.setHeader('Referrer-Policy', 'same-origin');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
   next();
 });
 
